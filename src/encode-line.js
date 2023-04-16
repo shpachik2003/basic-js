3f02b9c8bf9d626f41b2832b1694aee824600926
Kristina Naydenova <kristina.shpakovskaya@gmail.com>
2023-04-17 00:48:01 +0200

    init: encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,18 +1,18 @@
-const { NotImplementedError } = require('../extensions/index.js');
+function encodeLine(str) {
+  let encoded = '';
+  let count = 1;
 
-/**
- * Given a string, return its encoding version.
- *
- * @param {String} str
- * @return {String}
- *
- * @example
- * For aabbbc should return 2a3bc
- *
- */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  for (let i=1; i < str.length; i++){
+    if (str[i] === str[i-1]);
+    count++;
+  }
+    else {
+      encoded += (count > 1 ? count : '') + str[i - 1];
+      count = 1;
+    }
+  
+  
+  return encoded;
 }
 
 module.exports = {
