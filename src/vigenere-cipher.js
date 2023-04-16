969816f1faad1f5d0254dddfb9607b802c47914c
Kristina Naydenova <kristina.shpakovskaya@gmail.com>
2023-04-17 00:23:09 +0200

    init: vigenereCipherigMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,32 +1,66 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-/**
- * Implement class VigenereCipheringMachine that allows us to create
- * direct and reverse ciphering machines according to task description
- * 
- * @example
- * 
- * const directMachine = new VigenereCipheringMachine();
- * 
- * const reverseMachine = new VigenereCipheringMachine(false);
- * 
- * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
- * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
- * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
- * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
- */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const upperCaseMessage = message.toUpperCase().replace(/[^A-Z]/g, '');
+    const upperCaseKey = key.toUpperCase().replace(/[^A-Z]/g, '');
+
+    let encrypted = '';
+    let keyIndex = 0;
+    for (let i = 0; i < upperCaseMessage.length; i++) {
+      const charCode = upperCaseMessage.charCodeAt(i);
+      if (charCode >= 65 && charCode <= 90) {
+        const messageIndex = charCode - 65;
+        const keyCode = upperCaseKey.charCodeAt(keyIndex % upperCaseKey.length) - 65;
+        const encryptedCharCode = ((messageIndex + keyCode) % 26) + 65;
+        encrypted += String.fromCharCode(encryptedCharCode);
+        keyIndex++;
+      } else {
+        encrypted += upperCaseMessage[i];
+      }
+    }
+
+    if (!this.isDirect) {
+      encrypted = encrypted.split('').reverse().join('');
+    }
+
+    return encrypted;
+  }
+
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const upperCaseMessage = encryptedMessage.toUpperCase().replace(/[^A-Z]/g, '');
+    const upperCaseKey = key.toUpperCase().replace(/[^A-Z]/g, '');
+
+    let decrypted = '';
+    let keyIndex = 0;
+    for (let i = 0; i < upperCaseMessage.length; i++) {
+      const charCode = upperCaseMessage.charCodeAt(i);
+      if (charCode >= 65 && charCode <= 90) {
+        const messageIndex = charCode - 65;
+        const keyCode = upperCaseKey.charCodeAt(keyIndex % upperCaseKey.length) - 65;
+        const decryptedCharCode = ((messageIndex - keyCode + 26) % 26) + 65;
+        decrypted += String.fromCharCode(decryptedCharCode);
+        keyIndex++;
+      } else {
+        decrypted += upperCaseMessage[i];
+      }
+    }
+
+    if (!this.isDirect) {
+      decrypted = decrypted.split('').reverse().join('');
+    }
+
+    return decrypted;
   }
 }
 
