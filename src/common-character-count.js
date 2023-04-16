e34fd438930a2286adc4c25eab865c6829027d39
Kristina Naydenova <kristina.shpakovskaya@gmail.com>
2023-04-16 23:41:08 +0200

    init: commonCharacterCount

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,19 +1,14 @@
-const { NotImplementedError } = require('../extensions/index.js');
+function getCommonCharacterCount(s1, s2) {
+  let commonCount = 0;
+  let s1Array = s1.split('');
 
-/**
- * Given two strings, find the number of common characters between them.
- *
- * @param {String} s1
- * @param {String} s2
- * @return {Number}
- *
- * @example
- * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
- * Strings have 3 common characters - 2 "a"s and 1 "c".
- */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  for (let i=0; i < s1Array.length; i++){
+    if (s2.includes(s1Array[i])) {
+      commonCount++;
+      s2 = s2.replace(s1Array[i], '')
+    }
+  }
+  return commonCount;
 }
 
 module.exports = {
