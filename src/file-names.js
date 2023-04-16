8a0445b9405d72fd003cbc9809b1a57498b90801
Kristina Naydenova <kristina.shpakovskaya@gmail.com>
2023-04-17 01:05:27 +0200

    init: renameFiles

--- src/file-names.js
+++ src/file-names.js
@@ -1,23 +1,14 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-/**
- * There's a list of file, since two files cannot have equal names,
- * the one which comes later will have a suffix (k),
- * where k is the smallest integer such that the found name is not used yet.
- *
- * Return an array of names that will be given to the files.
- *
- * @param {Array} names
- * @return {Array}
- *
- * @example
- * For input ["file", "file", "image", "file(1)", "file"],
- * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
- *
- */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const counts = {};
+  return names.map(name => {
+    if (counts[name]) {
+      const newName = `${name}(${counts[name]})`;
+      counts[newName] = (counts[newName] || 0) + 1;
+      return newName;
+    }
+    counts[name] = (counts[name] || 0) + 1;
+    return name;
+  });
 }
 
 module.exports = {
