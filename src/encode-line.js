85196a63ef16641fe26fdd8eb8c8dc0f5f1e4891
Kristina Naydenova <kristina.shpakovskaya@gmail.com>
2023-04-17 00:51:18 +0200

    fix: encodeLine

--- src/encode-line.js
+++ src/encode-line.js
@@ -2,15 +2,14 @@ function encodeLine(str) {
   let encoded = '';
   let count = 1;
 
-  for (let i=1; i < str.length; i++){
-    if (str[i] === str[i-1]);
-    count++;
-  }
-    else {
+  for (let i = 1; i <= str.length; i++) {
+    if (str[i] === str[i - 1]) {
+      count++;
+    } else {
       encoded += (count > 1 ? count : '') + str[i - 1];
       count = 1;
     }
-  
+  }
   
   return encoded;
 }
