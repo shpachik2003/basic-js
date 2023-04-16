2fd4a6f67f4803f66c993cb1379ece923b91d520
Kristina Naydenova <kristina.shpakovskaya@gmail.com>
2023-04-16 23:31:17 +0200

    init: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,25 +1,20 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
-/**
- * Determine the age of archeological find by using
- * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
- * @return {Number | Boolean} calculated age in years or false
- * in case of incorrect sampleActivity
- *
- * @example
- * 
- * dateSample('1') => 22387
- * dateSample('WOOT!') => false
- *
- */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string" || isNaN(parseFloat(sampleActivity))) {
+    return false;
+  }
+  
+  const activity = parseFloat(sampleActivity);
+  if (activity <= 0 || activity >= MODERN_ACTIVITY) {
+    return false;
+  }
+  
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const age = Math.ceil(Math.log(MODERN_ACTIVITY / activity) / k);
+  
+  return age;
 }
 
 module.exports = {
