c71da467cb02a89862e744969153f81ab26d4d1c
Kristina Naydenova <kristina.shpakovskaya@gmail.com>
2023-04-17 01:26:04 +0200

    fix: hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,5 +1,5 @@
 function calculateHanoi(disksNumber, turnsSpeed) {
-  const turns = Math.pow(2, diskNumber) - 1;
+  const turns = Math.pow(2, disksNumber) - 1;
   const seconds = Math.floor(turns * 3600 / turnsSpeed);
   return { turns, seconds };
 }
