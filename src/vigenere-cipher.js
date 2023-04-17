2810d6c117aa54583d2591716af5b4eda482452c
Kristina Naydenova <kristina.shpakovskaya@gmail.com>
2023-04-18 01:00:42 +0200

    fix: vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,82 +1,58 @@
 class VigenereCipheringMachine {
-  constructor(direct = true) {
-    this.direct = direct;
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
 
   encrypt(message, key) {
     if (!message || !key) {
       throw new Error('Incorrect arguments!');
     }
-    
-    const messageUpperCase = message.toUpperCase().replace(/[^A-Z]/g, '');
-    const keyUpperCase = key.toUpperCase().replace(/[^A-Z]/g, '');
-    const vigenereTable = this.createVigenereTable();
 
     let result = '';
-    let keyIndex = 0;
-    for (let i = 0; i < messageUpperCase.length; i++) {
-      const messageCharCode = messageUpperCase.charCodeAt(i);
-      const keyCharCode = keyUpperCase.charCodeAt(keyIndex % keyUpperCase.length);
-      
-      if (messageCharCode < 65 || messageCharCode > 90) {
-        result += message[i];
-        continue;
-      }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let keyLength = key.length;
+    let j = 0;
 
-      const encryptedCharCode = vigenereTable[messageCharCode - 65][keyCharCode - 65];
-      result += String.fromCharCode(encryptedCharCode);
-      keyIndex++;
+    for (let i = 0; i < message.length; i++) {
+      let letter = message[i];
+
+      if (letter.charCodeAt(0) >= 65 && letter.charCodeAt(0) <= 90) {
+        let encryptedLetter = String.fromCharCode((letter.charCodeAt(0) + key.charCodeAt(j % keyLength)) % 26 + 65);
+        result += encryptedLetter;
+        j++;
+      } else {
+        result += letter;
+      }
     }
 
-    return this.direct ? result : result.split('').reverse().join('');
+    return this.isDirect ? result : result.split('').reverse().join('');
   }
 
-  decrypt(encryptedMessage, key) {
-    if (!encryptedMessage || !key) {
+  decrypt(message, key) {
+    if (!message || !key) {
       throw new Error('Incorrect arguments!');
     }
 
-    const encryptedMessageUpperCase = encryptedMessage.toUpperCase().replace(/[^A-Z]/g, '');
-    const keyUpperCase = key.toUpperCase().replace(/[^A-Z]/g, '');
-    const vigenereTable = this.createVigenereTable();
-
     let result = '';
-    let keyIndex = 0;
-    for (let i = 0; i < encryptedMessageUpperCase.length; i++) {
-      const encryptedCharCode = encryptedMessageUpperCase.charCodeAt(i);
-      const keyCharCode = keyUpperCase.charCodeAt(keyIndex % keyUpperCase.length);
-      
-      if (encryptedCharCode < 65 || encryptedCharCode > 90) {
-        result += encryptedMessage[i];
-        continue;
-      }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let keyLength = key.length;
+    let j = 0;
 
-      const messageCharCode = this.findDecryptedCharCode(vigenereTable, encryptedCharCode, keyCharCode);
-      result += String.fromCharCode(messageCharCode);
-      keyIndex++;
-    }
+    for (let i = 0; i < message.length; i++) {
+      let letter = message[i];
 
-    return this.direct ? result : result.split('').reverse().join('');
-  }
-
-  createVigenereTable() {
-    const table = [];
-    for (let i = 0; i < 26; i++) {
-      const row = [];
-      for (let j = 0; j < 26; j++) {
-        row.push((i + j) % 26 + 65);
-      }
-      table.push(row);
-    }
-    return table;
-  }
-
-  findDecryptedCharCode(vigenereTable, encryptedCharCode, keyCharCode) {
-    for (let i = 0; i < 26; i++) {
-      if (vigenereTable[i][keyCharCode - 65] === encryptedCharCode) {
-        return i + 65;
+      if (letter.charCodeAt(0) >= 65 && letter.charCodeAt(0) <= 90) {
+        let decryptedLetter = String.fromCharCode(((letter.charCodeAt(0) + 26) - key.charCodeAt(j % keyLength)) % 26 + 65);
+        result += decryptedLetter;
+        j++;
+      } else {
+        result += letter;
       }
     }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
   }
 }
 
